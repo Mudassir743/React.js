1b7982ea9319419279d84cba5ebb33376590af49
MUDASSIR AHMED <mudassirahmed9125gmail.com>
2025-09-17 19:49:43 +0500

    react

---
+++ src/App.tsx
@@ -0,0 +1,177 @@
+// import ListGroup from "./componenets/ListGroup";
+// function App() {
+//   let items = ["NewYourk", "London", "Paris", "Tokyo", "Turky"];
+//   let handleSelectItems = (item: string) => {
+//     console.log(item.length);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItems={handleSelectItems}
+//       />
+//     </div>
+//   );
+// }
+// export default App;
+// import { Alerts } from "./componenets/Alerts";
+// function App() {
+//   return (
+//     <div>
+//       <Alerts>
+//         "Hello <span> World" </span>
+//       </Alerts>
+//     </div>
+//   );
+// }
+
+// export default App;
+// import { useState } from "react";
+// import Alerts from "./components/Alerts";
+// import Button from "./components/Button";
+
+// function App() {
+//   let [alertVisible, setAlertVisible] = useState(false);
+//   return (
+//     <div>
+//       {alertVisible && (
+//         <Alerts onClose={() => setAlertVisible(false)}> My Alert</Alerts>
+//       )}
+//       <Button color="secondary" onClick={() => setAlertVisible(true)}>
+//         My Button
+//       </Button>{" "}
+//       <Button
+//         color="primary"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>
+//       {"   "}
+//       <Button
+//         color="success"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>{" "}
+//       <Button
+//         color="info"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>{" "}
+//       <Button
+//         color="light"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>{" "}
+//       <Button
+//         color="dark"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>{" "}
+//       <Button
+//         color="link"
+//         onClick={() => console.log("Button Touc kr ditta oyee")}
+//       >
+//         My Button
+//       </Button>{" "}
+//     </div>
+//   );
+// }
+// export default App;
+// import ListGroup from "./components/ListItems/ListGroup";
+// function App() {
+//   let items = ["NewYourk", "London", "Paris", "Tokyo", "Turky"];
+//   let handleSelectItems = (item: string) => {
+//     console.log(item.length);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItems={handleSelectItems}
+//       />
+//     </div>
+//   );
+// }
+// // export default App;
+import { useState } from "react";
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter App 🚀</h1>
+      <h2>Count: {count}</h2>
+      <div style={{ gap: "10px" }}>
+        <button
+          style={{
+            height: "35px",
+            width: "100px",
+            borderRadius: "18px",
+            border: "none",
+            fontFamily: "Monospace",
+            fontWeight: "bold",
+            fontSize: "large",
+            color: "white",
+            backgroundImage:
+              "linear-gradient(to right, #000046 0%, #1CB5E0 51%, #000046 100%)",
+            backgroundSize: "200% auto",
+            boxShadow: "0 0 20px #eee",
+            transition: "0.5s",
+          }}
+          onClick={() => setCount(count + 5)}
+        >
+          Increase
+        </button>
+        <button
+          style={{
+            height: "35px",
+            width: "100px",
+            borderRadius: "18px",
+            border: "none",
+            fontFamily: "Monospace",
+            fontWeight: "bold",
+            fontSize: "large",
+            color: "white",
+            backgroundImage:
+              "linear-gradient(to right, #000046 0%, #1CB5E0 51%, #000046 100%)",
+            backgroundSize: "200% auto",
+            boxShadow: "0 0 20px #eee",
+            transition: "0.5s",
+          }}
+          onClick={() => setCount(count - 5)}
+        >
+          Decrease
+        </button>
+        <button
+          style={{
+            height: "35px",
+            width: "100px",
+            borderRadius: "18px",
+            border: "none",
+            fontFamily: "Monospace",
+            fontWeight: "bold",
+            fontSize: "large",
+            color: "white",
+            backgroundImage:
+              "linear-gradient(to right, #000046 0%, #1CB5E0 51%, #000046 100%)",
+            backgroundSize: "200% auto",
+            boxShadow: "0 0 20px #eee",
+            transition: "0.5s",
+          }}
+          onClick={() => setCount((count = 0))}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListItems/ListGroup.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`  list-style: none;
+padding: 0px
+
+}`;
+
+interface ListItemsProps {
+  active: boolean;
+}
+const ListItems = styled.li<ListItemsProps>`
+  padding: 5px;
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItems: (item: string) => void;
+}
+function ListGroup({ items, heading, onSelectItems }: Props) {
+  let [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p> List not found</p>}
+      <List className={"list-group"}>
+        {items.map((item, index) => (
+          <ListItems
+            active={index === selectedIndex}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItems(item);
+            }}
+          >
+            {item}
+          </ListItems>
+        ))}
+      </List>
+    </>
+  );
+}
+export default ListGroup;
+
+//   const handleincreament = () => {
+//     setNumber(numbers + 1);
+//   };
+//   let [numbers, setNumber] = useState(0);
+//   return (
+//     <>
+//       <button onClick={handleincreament}>Click Here</button>
+//       <button onClick={() => setNumber(numbers - 1)}>Click Here</button>
+//       <button onClick={() => setNumber((numbers = 0))}>Click Here</button>
+//       {numbers}
+//     </>
+//   );
+// }
